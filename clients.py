04a0b7aecae18373fdb1ca031ed63c65ccb87050
Javier Besada <javierbesada88@gmail.com>
2020-11-23 08:39:43 +0100

    23/11/2020

--- clients.py
+++ clients.py
@@ -2,14 +2,14 @@ import var, conexion
 from ventana import *
 
 class Clientes():
-    '''
-    eventos clientes
-    '''
+    """
+    eventos necesarios formulario clientes
+    """
     def validarDni(dni):
-        '''
+        """
         Código que controla si el dni o nie es correcto
         :return:
-        '''
+        """
         try:
             tabla = 'TRWAGMYFPDXBNJZSQVHLCKE'
             dig_ext = 'XYZ'
@@ -20,18 +20,18 @@ class Clientes():
                 dig_control = dni[8]
                 dni = dni[:8]
                 if dni[0] in dig_ext:
-                    dni  = dni.replace(dni[0],reemp_dig_ext[dni[0]])
-                return len(dni) == len([n for n in dni if n in numeros]) and tabla[int(dni)%23 ] == dig_control
+                    dni  = dni.replace(dni[0], reemp_dig_ext[dni[0]])
+                return len(dni) == len([n for n in dni if n in numeros]) and tabla[int(dni)%23] == dig_control
 
-        except:
-            print('Error módulo validar DNI')
+        except Exception as error:
+            print('Error módulo validar DNI %s' % str(error))
             return None
 
     def validoDni():
-        '''
+        """
         muestra mensaje de dni válido
         :return: none
-        '''
+        """
         try:
             dni = var.ui.editDni.text()
             if Clientes.validarDni(dni):
@@ -47,7 +47,7 @@ class Clientes():
             print('Error módulo escribir valido DNI')
             return None
 
-    def selSexo():
+    def selSexo(self):
         try:
             if var.ui.rbtFem.isChecked():
                 var.sex =  'Mujer'
@@ -57,18 +57,20 @@ class Clientes():
             print('Error: %s' % str(error))
 
     def selPago():
+        '''
+        chequea que valores de pago han sido activados
+        agrupamos en QtDesigner los checkbox en un ButtonGroup
+        :return: devuelve una lista de valores
+        '''
         try:
             var.pay = []
             for i, data in enumerate(var.ui.grpbtnPay.buttons()):
-                #agrupamos en QtDesigner los checkbox en un ButtonGroup
                 if data.isChecked() and i == 0:
-                    var.pay.append('Efectivo')
+                   var.pay.append('Efectivo')
                 if data.isChecked() and i == 1:
-                    var.pay.append('Tarjeta')
+                   var.pay.append('Tarjeta')
                 if data.isChecked() and i == 2:
-                    var.pay.append('Transferencia')
-            #var.pay = set(var.pay)
-            #print (var.pay)
+                   var.pay.append('Transferencia')
             return var.pay
         except Exception as error:
             print('Error: %s' % str(error))
@@ -80,20 +82,19 @@ class Clientes():
         except Exception as error:
             print('Error: %s' % str(error))
 
-    '''
-    Abrir la ventana calendario
-    '''
-    def abrirCalendar():
+    def abrirCalendar(self):
+        '''
+        Abrir la ventana calendario
+        '''
         try:
             var.dlgcalendar.show()
         except Exception as error:
             print('Error: %s ' % str(error))
 
-    '''
-    Este módulo se ejecuta cuando clickeamos en un día del calendar, es decir, clicked.connect de calendar
-    '''
-
     def cargarFecha(qDate):
+        ''''
+        Este módulo se ejecuta cuando clickeamos en un día del calendar, es decir, clicked.connect de calendar
+        '''
         try:
             data = ('{0}/{1}/{2}'.format(qDate.day(), qDate.month(), qDate.year()))
             var.ui.editClialta.setText(str(data))
@@ -101,16 +102,18 @@ class Clientes():
         except Exception as error:
             print('Error cargar fecha: %s ' % str(error))
 
-    def showClientes():  #SE EJECUTA CON EL BOTÓN ACEPTAR
+    def altaCliente(self):  #SE EJECUTA CON EL BOTÓN ACEPTAR
         '''
-        cargará los clientes en la tabla
+        cargará los clientes en la tabla y en la base de datos
+        cargará datos cliente en el resto widgets
+        en las búsquedas mostrará los datos del cliente
         :return: none
         '''
         #preparamos el registro
         try:
-            newcli = []
+            newcli = [] #contiene todos los datos
             clitab = []  #será lo que carguemos en la tablas
-            client = [var.ui.editDni, var.ui.editApel, var.ui.editNome, var.ui.editClialta, var.ui.editDir ]
+            client = [var.ui.editDni, var.ui.editApel, var.ui.editNome, var.ui.editClialta, var.ui.editDir]
             k = 0
             for i in client:
                 newcli.append(i.text())  #cargamos los valores que hay en los editline
@@ -121,7 +124,6 @@ class Clientes():
             newcli.append(var.sex)
             var.pay2 = Clientes.selPago()
             newcli.append(var.pay2)
-            print(newcli)
             if client:
             #comprobarmos que no esté vacío lo principal
             #aquí empieza como trabajar con la TableWidget
@@ -132,68 +134,114 @@ class Clientes():
                     cell = QtWidgets.QTableWidgetItem(registro)
                     var.ui.tableCli.setItem(row, column, cell)
                     column +=1
-                conexion.Conexion.cargarCli(newcli)
+                conexion.Conexion.altaCli(newcli)
             else:
                 print('Faltan Datos')
-            Clientes.limpiarCli(client, var.rbtsex, var.chkpago)
+            #Clientes.limpiarCli()
         except Exception as error:
-            print('Error cargar fecha: %s ' % str(error))
+            print('Error cargar fecha lo : %s ' % str(error))
 
-    def limpiarCli(listaeditCli, listaRbtsex, listaChkpay):
+    def limpiarCli():
         '''
         limpia los datos del formulario cliente
-        :param listaRbtsex:
-        :param listaChkpay:
         :return: none
         '''
         try:
-            for i in range(len(listaeditCli)):
-                listaeditCli[i].setText('')
+            client = [var.ui.editDni, var.ui.editApel, var.ui.editNome, var.ui.editClialta, var.ui.editDir]
+            for i in range(len(client)):
+                client[i].setText('')
             var.ui.grpbtnSex.setExclusive(False)  #necesario para los radiobutton
-            for dato in listaRbtsex:
+            for dato in var.rbtsex:
                 dato.setChecked(False)
-            for data in listaChkpay:
+            for data in var.chkpago:
                 data.setChecked(False)
             var.ui.cmbProv.setCurrentIndex(0)
             var.ui.lblValidar.setText('')
+            var.ui.lblCodcli.setText('')
         except Exception as error:
             print('Error limpiar widgets: %s ' % str(error))
 
-    def cargarCli(self):
+    def cargarCli():
+        '''
+        carga en widgets formulario cliente los datos
+        elegidos en la tabla
+        :return: none
+        '''
         try:
             fila = var.ui.tableCli.selectedItems()
             client = [ var.ui.editDni, var.ui.editApel, var.ui.editNome ]
             if fila:
                 fila = [dato.text() for dato in fila]
-            print(fila)
             i = 0
             for i, dato in enumerate(client):
                 dato.setText(fila[i])
+            conexion.Conexion.cargarCliente()
+        except Exception as error:
+            print('Error cargar clientes: %s ' % str(error))
 
+    def bajaCliente(self):
+        """
+        módulos para dar de baja un cliente
+        :return:
+        """
+        try:
+            dni = var.ui.editDni.text()
+            conexion.Conexion.bajaCli(dni)
+            conexion.Conexion.mostrarClientes(self)
+            Clientes.limpiarCli()
         except Exception as error:
             print('Error cargar clientes: %s ' % str(error))
 
 
+    def modifCliente(self):
+        """Módulos para modificar datos de un cliente con determinado código
+
+        :return: None
+        """
+        try:
+            newdata = []
+            client = [var.ui.editDni, var.ui.editApel, var.ui.editNome, var.ui.editClialta, var.ui.editDir]
+            for i in client:
+                newdata.append(i.text())  # cargamos los valores que hay en los editline
+            newdata.append(var.ui.cmbProv.currentText())
+            newdata.append(var.sex)
+            var.pay = Clientes.selPago()
+            newdata.append(var.pay)
+            cod = var.ui.lblCodcli.text()
+            conexion.Conexion.modifCli(cod, newdata)
+            conexion.Conexion.mostrarClientes(self)
+
+        except Exception as error:
+            print('Error cargar clientes: %s ' % str(error))
+
+    def reloadCli():
+        '''
+        Limpia datos formulario y recarga la tabla de clientes
+        :return: None
+        '''
+        try:
+            print(var.ui.spinEdad.value())
+            Clientes.limpiarCli()
+            conexion.Conexion.mostrarClientes(None)
+        except Exception as error:
+            print('Error recargar clientes: %s ' % str(error))
+
+    def buscarCli(self):
+        """
+        Busca un Cliente a partir de un dni que escribe el usuario
+        :return: mensaje
+        """
+        try:
+            #Clientes.limpiarCli()
+            dni = var.ui.editDni.text()
+            conexion.Conexion.buscaCli(dni)
+        except Exception as error:
+            print('Error recargar clientes: %s ' % str(error))
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def valoresSpin():
+        try:
+            var.ui.spinEdad.setValue(16)
+        except Exception as error:
+            print('Error valores spin: %s ' % str(error))
--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from vencalendar import *
 from datetime import datetime
 import sys, var, events, clients, conexion
 
+
 class DialogSalir(QtWidgets.QDialog):
     def __init__(self):
         super(DialogSalir, self).__init__()
         var.dlgsalir = Ui_dlgSalir()
         var.dlgsalir.setupUi(self)
         var.dlgsalir.btnBoxSalir.button(QtWidgets.QDialogButtonBox.Yes).clicked.connect(events.Eventos.Salir)
-        #var.dlgsalir.btnBoxSalir.button(QtWidgets.QDialogButtonBox.No).clicked.connect(events.Eventos.closeSalir)
-        #no es neceasario no quiero que haga nada
+        # var.dlgsalir.btnBoxSalir.button(QtWidgets.QDialogButtonBox.No).clicked.connect(events.Eventos.closeSalir)
+        # no es neceasario no quiero que haga nada
+
 
 class DialogCalendar(QtWidgets.QDialog):
     def __init__(self):
@@ -21,8 +23,10 @@ class DialogCalendar(QtWidgets.QDialog):
         diaactual = datetime.now().day
         mesactual = datetime.now().month
         anoactual = datetime.now().year
-        var.dlgcalendar.Calendar.setSelectedDate((QtCore.QDate(anoactual,mesactual,diaactual)))
+        var.dlgcalendar.Calendar.setSelectedDate((QtCore.QDate(anoactual, mesactual, diaactual)))
         var.dlgcalendar.Calendar.clicked.connect(clients.Clientes.cargarFecha)
+
+
 
 class Main(QtWidgets.QMainWindow):
     def __init__(self):
@@ -37,37 +41,47 @@ class Main(QtWidgets.QMainWindow):
         '''
         var.rbtsex = (var.ui.rbtFem, var.ui.rbtMasc)
         var.chkpago = (var.ui.chkEfec, var.ui.chkTar, var.ui.chkTrans)
-
         '''
         conexion de eventos con los objetos
         estamos conectando el código con la interfaz gráfico
+        botones formulario cliente
         '''
-
         var.ui.btnSalir.clicked.connect(events.Eventos.Salir)
         var.ui.actionSalir.triggered.connect(events.Eventos.Salir)
-        var.ui.editDni.editingFinished.connect(clients.Clientes.validoDni)
+        var.ui.editDni.editingFinished.connect(lambda: clients.Clientes.validoDni())
         var.ui.btnCalendar.clicked.connect(clients.Clientes.abrirCalendar)
-        var.ui.btnAceptar.clicked.connect(clients.Clientes.showClientes)
+        var.ui.btnAltaCli.clicked.connect(clients.Clientes.altaCliente)
+        var.ui.btnLimpiarCli.clicked.connect(clients.Clientes.limpiarCli)
+        var.ui.btnBajaCli.clicked.connect(clients.Clientes.bajaCliente)
+        var.ui.btnModifCli.clicked.connect(clients.Clientes.modifCliente)
+        var.ui.btnReloadCli.clicked.connect(clients.Clientes.reloadCli)
+        var.ui.btnBuscarCli.clicked.connect(clients.Clientes.buscarCli)
+        clients.Clientes.valoresSpin()
+
         for i in var.rbtsex:
             i.toggled.connect(clients.Clientes.selSexo)
         for i in var.chkpago:
-             i.stateChanged.connect(clients.Clientes.selPago)
+            i.stateChanged.connect(clients.Clientes.selPago)
+
         var.ui.cmbProv.activated[str].connect(clients.Clientes.selProv)
         var.ui.tableCli.clicked.connect(clients.Clientes.cargarCli)
         var.ui.tableCli.setSelectionBehavior(QtWidgets.QTableWidget.SelectRows)
-        '''
-        Llamada a módulos iniciales
-        '''
         events.Eventos.cargarProv()
+        var.ui.statusbar.addPermanentWidget(var.ui.lblstatus, 1)
+        var.ui.lblstatus.setText('Bienvenido a 2º DAM')
 
         '''
-        módulos del principal
+        módulos conexion base datos
         '''
+
         conexion.Conexion.db_connect(var.filebd)
+        # conexion.Conexion()
+        conexion.Conexion.mostrarClientes(self)
 
     def closeEvent(self, event):
         if event:
             events.Eventos.Salir(event)
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
